47d810f612023a69e3200d284d1b663049bfe839
natasaradenovic <nradenovic93@gmail.com>
2018-02-03 16:19:38 +0100

    Call productservice & offerservice simultaneously

--- aggregator/src/main/java/com/bol/test/assignment/aggregator/AggregatorService.java
+++ aggregator/src/main/java/com/bol/test/assignment/aggregator/AggregatorService.java
@@ -1,5 +1,7 @@
 package com.bol.test.assignment.aggregator;
 
+import java.util.concurrent.ExecutionException;
+
 import com.bol.test.assignment.offer.Offer;
 import com.bol.test.assignment.offer.OfferCondition;
 import com.bol.test.assignment.offer.OfferService;
@@ -7,11 +9,12 @@ import com.bol.test.assignment.order.Order;
 import com.bol.test.assignment.order.OrderService;
 import com.bol.test.assignment.product.Product;
 import com.bol.test.assignment.product.ProductService;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import lombok.Getter;
 
 public class AggregatorService {
+
+    private static int TIME_OUT = 2000;
+
     private OrderService orderService;
     private OfferService offerService;
     private ProductService productService;
@@ -23,10 +26,76 @@ public class AggregatorService {
     }
 
     public EnrichedOrder enrich(int sellerId) throws ExecutionException, InterruptedException {
-       return null;
+        final Order order = orderService.getOrder(sellerId);
+
+        final FetcherThread fetcherThread = new FetcherThread(order);
+        fetcherThread.start();
+
+        try {
+            fetcherThread.join(TIME_OUT);
+        } catch (InterruptedException e) {
+            System.err.println("Thread 'fetcher' interrupted\n");
+        }
+
+        final Product product = fetcherThread.getProduct();
+        final Offer offer = fetcherThread.getOffer();
+
+        return combine(order, offer, product);
     }
 
     private EnrichedOrder combine(Order order, Offer offer, Product product) {
         return new EnrichedOrder(order.getId(), offer.getId(), offer.getCondition(), product.getId(), product.getTitle());
+    }
+
+    /**
+     * New thread for fetching product and offer simultaneously.
+     */
+    class FetcherThread extends Thread {
+
+        private Order order;
+
+        @Getter
+        private Product product;
+        @Getter
+        private Offer offer;
+
+        protected FetcherThread(final Order order) {
+            this.order = order;
+
+            // Set default values.
+            product = new Product(-1, null);
+            offer = new Offer(-1, OfferCondition.UNKNOWN);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Running thread 'fetcher' for order id " +  order.getId());
+
+            final Thread productThread = new Thread(() -> {
+                System.out.println("    Fetching product with id " +  order.getProductId());
+                product = productService.getProduct(order.getProductId());
+                System.out.println("    Thread 'product' exiting.");
+            });
+
+            final Thread offerThread = new Thread(() -> {
+                System.out.println("    Fetching offer with id " +  order.getOfferId());
+                offer = offerService.getOffer(order.getOfferId());
+                System.out.println("    Thread 'offer' exiting.");
+            });
+
+            // Start threads simultaneously.
+            productThread.start();
+            offerThread.start();
+
+            try {
+                // Wait for both threads to complete.
+                productThread.join(TIME_OUT);
+                offerThread.join(TIME_OUT);
+            } catch (InterruptedException e) {
+                System.err.println("Thread(s) interrupted");
+            }
+
+            System.out.println("Thread 'fetcher' exiting\n");
+        }
     }
 }
